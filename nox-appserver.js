72ef11762f0d65a0a5811a8d1f2a24e5cc31cb6e
Antti Saarinen <antti.p.saarinen@gmail.com>
2013-09-16 13:36:36 +0300

    moved ports and listening out of nox-appserver added useBefore() pass requirefun and logfun to appserver

--- nox-appserver.js
+++ nox-appserver.js
@@ -1,13 +1,14 @@
-module.exports = function(appname, port) {
+module.exports = function(appname, requirefun, logfun) {
     
+    if( typeof logfun != 'function' )
+	logfun = function() {}
+
     var http = require('http');
     var express = require('express');
     var async = require('async');
     
     var expressServer = express();
     var httpServer = http.createServer(expressServer);
-    
-    var log = require('./lib/log.js');
 
     expressServer.use(express.cookieParser());
     expressServer.use(express.session({    
@@ -16,13 +17,22 @@ module.exports = function(appname, port) {
 	cookie: { path: '/', httpOnly: true, maxAge: 100000000 }
     }));
 
+    var usedFuns = [];
+    expressServer.use(function(req, res, next) {
+	async.eachSeries(usedFuns, function(fun, funcb) {
+	    fun(req, res, function(err) {
+		funcb(err);
+	    });
+	}, next);
+    });
+
     expressServer.use(expressServer.router);
     expressServer.use('/', express.static(__dirname + '/html'));
     
     var noxapp = require('nox').nox(
-	function(str) { return require(str); },
+	requirefun,
 	'nox-app-' + appname + '.sid', 
-	function(str) { log(str); });
+	logfun);
     
     expressServer.get('/js/nox-client.js', noxapp.get);
     
@@ -30,21 +40,9 @@ module.exports = function(appname, port) {
     socketServer.set('authorization', noxapp.socketAuth);
     socketServer.on('connection', noxapp.socketConn);
     
-    if( typeof port != 'number' )
-	port = 8080;
-    
-    log('starting server at port ' + port);
-    var serverinst = httpServer.listen(port);
-    
-    process.on('SIGINT', function() {
-	log('stopping server gracefully');
-	serverinst.close();
-	process.on('exit', function() {
-	    log('server stopped gracefully');
-	});
-    });
-
-    log('listening at ' + port);
+    expressServer.useBefore = function(fun) {
+	usedFuns.push(fun);
+    }
 
     return {
 	noxApp: noxapp,
--- nox-start.js
+++ nox-start.js
@@ -1,12 +1,34 @@
 var express = require('express');
 
-var app = require('./nox-appserver.js')('template', process.argv[2]);
+var log = require('./lib/log.js');
+
+var app = require('./nox-appserver.js')('template', 
+					function(mod) { return require(mod); }, log);
 
 app.noxApp.page('/login.html', [ './lib/auth.js' ], [ './lib/login.js' ]);
 app.noxApp.page('/index.html', [ './lib/auth.js', './lib/app.js' ], 
 		[ './lib/index.js' ]);
 
 app.expressServer.use('/', express.static('app'));
+
+var port = 8080;
+if( typeof process.argv[2] == 'number' )
+    port = parseInt(process.argv[2]);
+    
+log('starting server at port ' + port);
+var serverinst = app.httpServer.listen(port);
+
+process.on('SIGINT', function() {
+    log('stopping server gracefully');
+    serverinst.close();
+    process.on('exit', function() {
+	log('server stopped gracefully');
+    });
+});
+
+log('listening at ' + port);
+
+
 
 
 
